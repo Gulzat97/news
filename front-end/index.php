8c39d0fbe1ef75a03caf715c948bec4e6c0bb2ef
mio-eldar <mr.empoty.life@Gmail.com>
2018-04-27 22:17:37 +0600

    Update index.php

--- front-end/index.php
+++ front-end/index.php
@@ -1,1 +1,43 @@
+<?php require_once 'inc/header.php' ?>
+<?php require_once 'inc/slider.php' ?>
+	
 
+	<div class="main">
+		<div class="main-holder">
+			<div class="posts">
+				<?php
+					for($i = 0; $i < 5; $i++) {
+						echo
+							"<div class='post'>
+								<a href='post.php'>
+										<img class='post-img' src='/img/$i.jpg' alt=''>
+								</a>
+								<div class='post-text'>
+									<a href='/post.php'>
+										<h2 class='post-title'>
+											Рыбатекст " . ($i + 1) . "
+										</h2>
+									</a>
+									
+									<div class='post-intro'>
+										Значимость этих проблем настолько очевидна, что
+										начало повседневной работы по формированию
+										позиции требуют от нас анализа системы обучения
+										кадров, соответствует насущным потребностям.
+									</div>
+									<div class='post-date'>
+										27/05/2017
+									</div>
+								</div>
+							</div>";
+					}
+						
+				?>
+				
+			</div>
+			
+		</div>
+	</div>
+	
+
+<?php require_once 'inc/footer.php' ?>
